c96a00ae7901d81e4638c2bd0560f0fc48088716
Christopher J Mallery <zoggins@retro-spy.com>
2023-11-18 16:42:18 -0800

    CPM changes

---
+++ Z80-S100/cpm3-banked-serial-zfdc-source/Build_CF_img.bat
@@ -0,0 +1,38 @@
+Rem *** Delete previous image ***
+
+del cpm3-banked-prop-zdfc.img
+
+Rem *** Copy system files from build folder ***
+
+copy ZSYSGEN\CPMLDR.COM CPMLDR.COM
+copy "ABCD Floppys Banked CPM3"\CPM3.SYS CPM3.SYS
+
+Rem *** Make new file system image ***
+
+mkfs.cpm -f ibm-3740 -b CPMLDR.COM -t cpm3-banked-prop-zdfc.img
+
+
+
+Rem *** Copy system files ***
+
+cpmcp -f ibm-3740 cpm3-banked-prop-zdfc.img cpm3.sys 0:
+cpmcp -f ibm-3740 cpm3-banked-prop-zdfc.img ccp.com 0:
+cpmchattr -f ibm-3740 cpm3-banked-prop-zdfc.img s ccp.com 0:
+
+
+
+Rem *** Copy user files ***
+
+REM cpmcp -f s100ide cpm3-banked-prop.img ./CPM3-BASE/*.* 0:
+cpmcp -f ibm-3740 cpm3-banked-prop-zdfc.img ./S100-TOOLS/*.* 1:
+REM cpmcp -f s100ide cpm3-banked-prop.img ./WS4-FILES/*.* 2:
+REM cpmcp -f s100ide cpm3-banked-prop.img ./BASIC-FILES/*.* 3:
+cpmcp -f ibm-3740 cpm3-banked-prop-zdfc.img ./GAMES/*.* 4:
+
+pause
+
+
+Rem *** Clean up ***
+
+del CPMLDR.COM
+del CPM3.SYS
