39a5dc759acd9f78920bf9e131cddecdd567f193
Christopher J Mallery <zoggins@retro-spy.com>
2024-09-10 13:24:00 -0700

    Keyboard Input works with Propeller board

---
+++ Z80-S100/DAZZLER_GAMES/PROP_INPUT/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ z80asm -i 4DTICTAC.ASM -o 4DTICTAC.COM --list=4DTICTAC.LST
+ z80asm -i DAZCHESS.ASM -o DAZCHESS.COM --list=DAZCHESS.LST
+ z80asm -i LIFE.ASM -o LIFE.COM --list=LIFE.LST
+ z80asm -i X-LIFE.ASM -o X-LIFE.COM --list=X-LIFE.LST
+
+
+
+ 
